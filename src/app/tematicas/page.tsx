c933eb67dd120c681babbc2966e918c910714d87
Lucas Peixoto <lspeixotodev@gmail.com>
2023-10-27 22:20:19 -0300

    coming soon alert

--- src/app/tematicas/page.tsx
+++ src/app/tematicas/page.tsx
@@ -1,9 +1,10 @@
-import { title } from '@/components/primitives';
+import Image from 'next/image';
 
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>About</h1>
+      <Image src="/coming-soon.svg" alt="Em breve" width={300} height={200} />
+      <div className="my-4 text-xl text-black dark:text-white">Em breve</div>
     </div>
   );
 }
